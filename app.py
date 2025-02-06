c2e3b68c4841e0153651e0a7874af9c4aa15932a
Franck F <franck-dilane1.fambou@epitech.digital>
2025-02-06 15:07:34 +0100

    Update app.py
    Signed-off-by: Franck F <franck-dilane1.fambou@epitech.digital>

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 import os
 import re
 from PIL import Image
-import cv2
+#import cv2
 # Charger le fichier .env contenant la clé API
 #load_dotenv()
 #openai.api_key = os.getenv("OPENAI_API_KEY")
